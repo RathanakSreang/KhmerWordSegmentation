2fa862ed332bc4d1437e76c3ff629e482c193d14
Sreang Rathanak <sreangrathanak@yahoo.com>
2018-08-22 23:05:11 +0700

    Simple segmentation

--- word_segmentation.py
+++ word_segmentation.py
@@ -1,3 +1,19 @@
-# class WordSegmentation:
-#   def __init__(self):
-#     return False
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import trie
+from codecs import open, decode
+model = trie.Trie()
+model.load_from_pickle("train_data")
+
+kh_str = "អ្នកចេះនិយាយភាសាខ្មែរទេ?"
+
+words = []
+word = ''
+for ch in kh_str:
+  word += ch
+
+  if model.searchWord(word.strip()):
+    words.append(word)
+    print(word.strip())
+    word = ''
